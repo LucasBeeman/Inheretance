c422d3bfde0899e8f32e13c71e8a633d80e367de
Lucas Beeman <lucasbeeman12@gmail.com>
2021-10-13 10:37:16 -0400

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheretance
+{
+    public class Car : IAutomobile<Car>
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+
+        public bool Equals(Car car)
+        {
+            return (this.Make, this.Model, this.Year) ==
+                (car.Make, car.Model, car.Year);
+        }
+    }
+}
---
+++ MyInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheretance
+{
+    interface IAutomobile<T>
+    {
+        bool Equals(T obj);
+    }
+}
